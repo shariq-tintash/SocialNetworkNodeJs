1b8262458acca80204bf5dddae9f55a066a88465
shariq-tintash <shariq.rashid@tintash.com>
2022-08-03 23:38:03 +0500

    Initial Setup Changes

--- app.js
+++ app.js
@@ -2,30 +2,38 @@
 // External imports
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+
 // Internal imports
 
 // Environment variables
-
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
   process.env.MONGO_PASSWORD
-}@cluster0-ntrwp.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+}@socialnetworknodejs.a2k6a.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 
 // Initiliazing Express App
 const app = express();
+
 // Connecting to MongoDb database
-
-
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
     // https
     //   .createServer({ key: privateKey, cert: certificate }, app)
     //   .listen(process.env.PORT || 3000);
-      app.listen(process.env.PORT || 3000);
+      app.listen(process.env.SERVER_PORT);
+      console.log(`Server listening on PORT ${process.env.SERVER_PORT}`);
   })
   .catch(err => {
     console.log(err);
   });
+
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 // Routing
 
