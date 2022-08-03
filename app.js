97478017b464f65893306bcd8981501f1287507b
shariq-tintash <shariq.rashid@tintash.com>
2022-08-03 23:07:00 +0500

    Initial Setup of App.js

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 
 // External imports
-
+const express = require('express');
+const mongoose = require('mongoose');
 // Internal imports
 
-// Initiliazing Express App
+// Environment variables
 
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${
+  process.env.MONGO_PASSWORD
+}@cluster0-ntrwp.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
+// Initiliazing Express App
+const app = express();
 // Connecting to MongoDb database
+
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    // https
+    //   .createServer({ key: privateKey, cert: certificate }, app)
+    //   .listen(process.env.PORT || 3000);
+      app.listen(process.env.PORT || 3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 // Routing
 
